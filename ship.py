e896754d29c08e96bd206241ea6de3888cf2a3ff
Ligor <fornet11@mail.ru>
2020-04-12 11:36:28 +0300

    исправлен класс Ship

--- ship.py
+++ ship.py
@@ -1,15 +1,22 @@
 import pygame
 
+
 class Ship():
     def __init__(self, screen):
+        """Initialize the ship and set its starting position."""
         self.screen = screen
-
+         # Load the ship image and get its rect.
         self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
+
         self.screen_rect = screen.get_rect()
 
-        self.rect.centerx = self.screen_rect.centrex
+        # Start each new ship at the bottom center of the screen.
+        self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
+
+        # Store a decimal value for the ship's center
+        self.center = self.rect.centerx
 
     def blitme(self):
         self.screen.blit(self.image, self.rect)
