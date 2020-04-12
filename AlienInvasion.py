597366c641f8176bc88c1fd92a3ce1155042d0df
Ligor <fornet11@mail.ru>
2020-04-12 11:03:33 +0300

    добавлен класс Ship

--- AlienInvasion.py
+++ AlienInvasion.py
@@ -1,6 +1,7 @@
 import sys
 import pygame
 from settings import Settings
+from ship import Ship
 
 
 def run_game():
@@ -8,12 +9,14 @@ def run_game():
     ai_settings = Settings()
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_heigth))
     pygame.display.set_caption("Alien Invasion")
+    ship = Ship(screen)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
         screen.fill(ai_settings.bg_color)
+        ship.blitme()
         pygame.display.flip()
 
 
